ff5bb596f6243020740d3ac201d0b64da8a99be2
Kelsey Porter <porterke6@gmail.com>
2022-11-19 13:31:47 -0600

    commented code

--- Program.cs
+++ Program.cs
@@ -23,11 +23,16 @@ if (option == "1")
 else if (option == "2")
 {
     //Battle _battle = new Battle();
+
+    //new Entity(name, HP, max HP, MP, max MP, attack, magic attack, defense, magic defense, speed)
+
     List<Entity> enemies = new List<Entity>();
     enemies.Add(new Entity("Dragon", 200, 200, 0, 0, 10, 15, 0, 0, 5));
+
     List<Entity> squad = new List<Entity>();
     squad.Add(new Entity("Nobody", 100, 100, 10, 10, 5, 10, 10, 12, 10));
     squad.Add(new Entity("Cat", 50, 50, 0, 0, 5, 10, 5, 7, 15));
+
     Battle _battle = new Battle(enemies, squad);
     _battle.Run();
 }
