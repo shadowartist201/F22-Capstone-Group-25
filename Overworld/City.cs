82c0401fb994361081ae81b8d8ad4f8c3cc3a213
stevenamathis <113269862+stevenamathis@users.noreply.github.com>
2023-05-06 21:31:21 -0500

    Add NPCs to city

--- Overworld/City.cs
+++ Overworld/City.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 //using Microsoft.Xna.Framework.Audio;
 using MonoGame.Extended;
@@ -18,6 +18,11 @@ namespace Game_Demo
         private OrthographicCamera _camera;
 
         private EntityTest NPCHcityChest = new(null, new Vector2(885, 1791), false, false);
+        private EntityTest City_NPC1 = new(null, new Vector2(760, 1825), false, false);
+        private EntityTest City_NPC2 = new(null, new Vector2(1350, 280), false, false);
+
+        private bool talkToNPC1 = false;
+        private bool talkToNPC2 = false;
 
 
         private bool talkToNPCcityChest = false;
@@ -39,6 +44,8 @@ namespace Game_Demo
             _camera.LookAt(Tiled.startingPosition); //set starting position
 
             NPCHcityChest.sprite = Content.Load<Texture2D>("World/chest");
+            City_NPC1.sprite = Content.Load<Texture2D>("World/Village1_NPC2");
+            City_NPC2.sprite = Content.Load<Texture2D>("World/npc3");
 
             World.LoadAnim(Content);
             //_dialog.MakeBox(DialogText.Demo, Game1.DialogFont, GraphicsDevice, new OrthographicCamera(GraphicsDevice));
@@ -59,6 +66,30 @@ namespace Game_Demo
             Tiled.Update_(gameTime); //tiledMapRenderer update
             Tiled.currentPosition = _camera.Center;
             Transition.TransitionCheck();
+
+            if (Collision.CollisionCheck_Entity(City_NPC1) == Color.Blue && talkToNPC1 == false) //if near NPC1 and not spoken to
+                if (Input.SinglePress() == "enter")
+                {
+                    talkToNPC1 = true; //set flag to true
+                    City_NPC1.MakeDialogBox(DialogText.City_NPC1, GraphicsDevice); //make box
+                }
+            if (talkToNPC1) //if flag is true
+                if (City_NPC1.DialogUpdate() == "hidden") //when box is closed
+                    talkToNPC1 = false; //clear flag
+                else
+                    City_NPC1.DialogUpdate(); //update box
+
+            if (Collision.CollisionCheck_Entity(City_NPC2) == Color.Blue && talkToNPC2 == false) //if near NPC1 and not spoken to
+                if (Input.SinglePress() == "enter")
+                {
+                    talkToNPC2 = true; //set flag to true
+                    City_NPC2.MakeDialogBox(DialogText.City_NPC2, GraphicsDevice); //make box
+                }
+            if (talkToNPC2) //if flag is true
+                if (City_NPC2.DialogUpdate() == "hidden") //when box is closed
+                    talkToNPC2 = false; //clear flag
+                else
+                    City_NPC2.DialogUpdate(); //update box
 
             if (Collision.CollisionCheck_Entity(NPCHcityChest) == Color.Blue && talkToNPCcityChest == false) //if near NPC3 and not spoken to
                 if (Input.SinglePress() == "enter")
@@ -85,6 +116,11 @@ namespace Game_Demo
                     NPCHcityChest.DialogUpdate(); //update box
 
             if (Collision.CollisionCheck_Entity(NPCHcityChest) == Color.Green)
+                return;
+
+            if (Collision.CollisionCheck_Entity(City_NPC1) == Color.Green)
+                return;
+            if (Collision.CollisionCheck_Entity(City_NPC2) == Color.Green)
                 return;
 
             if (Collision.CollisionCheck() == Color.Green) //if collided
@@ -124,6 +160,8 @@ namespace Game_Demo
             //_spriteBatch.Draw(World.player, new Rectangle((int)_camera.Center.X, (int)_camera.Center.Y, Tiled.tileWidth, Tiled.tileWidth), Color.White);
 
             _spriteBatch.Draw(NPCHcityChest.sprite, new Rectangle((int)NPCHcityChest.position.X, (int)NPCHcityChest.position.Y, Tiled.tileWidth, Tiled.tileWidth), Color.White);
+            _spriteBatch.Draw(City_NPC1.sprite, new Rectangle((int)City_NPC1.position.X, (int)City_NPC1.position.Y, Tiled.tileWidth, Tiled.tileWidth), Color.White);
+            _spriteBatch.Draw(City_NPC2.sprite, new Rectangle((int)City_NPC2.position.X, (int)City_NPC2.position.Y, Tiled.tileWidth, Tiled.tileWidth), Color.White);
 
             World.DrawAnim(_spriteBatch);
             _spriteBatch.End();
@@ -135,6 +173,10 @@ namespace Game_Demo
 
             if (talkToNPCcityChest)
                 NPCHcityChest.DialogDraw(_spriteBatch);
+            if (talkToNPC1)
+                City_NPC1.DialogDraw(_spriteBatch);
+            if (talkToNPC2)
+                City_NPC2.DialogDraw(_spriteBatch);
 
             _spriteBatch.End();
         }
